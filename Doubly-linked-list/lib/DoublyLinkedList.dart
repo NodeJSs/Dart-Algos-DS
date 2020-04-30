884c94447cd0f4b991470dfa2dd7d32861160410
Yusuff Faruq <faruqyusuff437@gmail.com>
2020-04-30 19:27:11 +0000

    finished doubly-linked-list

--- Doubly-linked-list/lib/DoublyLinkedList.dart
+++ Doubly-linked-list/lib/DoublyLinkedList.dart
@@ -1,45 +1,132 @@
+import 'dart:io';
+
 import 'Node.dart';
 
-class DoublyLinkedList{
+class DoublyLinkedList {
   Node head;
+  Node tail;
 
-  DoublyLinkedList(dynamic headData){
-    this.head = Node(data:headData);
+  DoublyLinkedList(dynamic headData) {
+    Node newNode = Node(data: headData);
+    this.head = newNode;
+    this.tail = newNode;
     this.head.nextNode = null;
     this.head.prevNode = null;
+    this.tail.nextNode = null;
+    this.tail.prevNode = null;
   }
 
-  void insertAtFirst(dynamic data){
+  void insertAtFirst(dynamic data) {
     Node newNode = Node(data: data);
     newNode.nextNode = this.head;
     newNode.prevNode = null;
     this.head.prevNode = newNode;
-    this.head = newNode;  
+    this.head = newNode;
   }
 
-  void insertAtLast(){
+  void insertAtLast(dynamic data) {
+    Node newNode = Node(data: data);
 
+    newNode.nextNode = null;
+    if (this.head.nextNode == null) {
+      this.head.nextNode = newNode;
+      newNode.prevNode = this.head;
+      newNode.nextNode = null;
+      this.tail = newNode;
+    } else {
+      this.tail.nextNode = newNode;
+      newNode.prevNode = this.tail;
+      this.tail = newNode;
+    }
   }
 
-  void insertAtPosition(){
-
+  void insertAtPosition(dynamic data, int position) {
+    
+    if (position == 1) {
+      insertAtFirst(data);
+      }else {
+      Node nodeBeforeNewNode = getNodeAt(position - 1);
+      Node newNode = Node(data: data);
+      newNode.prevNode = nodeBeforeNewNode;
+      newNode.nextNode = nodeBeforeNewNode.nextNode;
+      newNode.nextNode.prevNode = newNode;
+      nodeBeforeNewNode.nextNode = newNode;
+    }
+    
   }
 
-  int get count => 0;
-
-  void deleteFirst(){
-
+  int get count {
+    int counter = 0;
+    Node currentNode = this.head;
+    while (currentNode != null) {
+      currentNode = currentNode.nextNode;
+      counter++;
+    }
+    return counter;
   }
 
-  void deleteLast(){
+  Node getNodeAt(int position) {
+    //Not zero-based unlike arrays
+    if (position > count || position <= 0) {
+      throw "Out of bounds";
+    }
 
+    Node currentNode = this.head;
+    int counter = 1;
+
+    while (counter < position) {
+      currentNode = currentNode.nextNode;
+      counter++;
+    }
+    return currentNode;
   }
 
-  void deleteAtPosition(){
-
+  void deleteFirst() {
+    if (this.head.nextNode == null) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head.nextNode.prevNode = null;
+      this.head = this.head.nextNode;
+    }
   }
 
+  void deleteLast() {
+    if (this.head.nextNode == null) {
+      this.head = null;
+      this.tail = null;
+    }
+    this.tail.prevNode.nextNode = null;
+    this.tail = this.tail.prevNode;
+  }
 
+  void deleteAtPosition(int position) {
+    if (position == 1) {
+      deleteFirst();
+    } else if (position == count) {
+      deleteLast();
+    } else {
+      Node nodeBeforeNodeToBeDeleted = getNodeAt(position - 1);
 
-  
+      nodeBeforeNodeToBeDeleted.nextNode =
+          nodeBeforeNodeToBeDeleted.nextNode.nextNode;
+      nodeBeforeNodeToBeDeleted.nextNode.prevNode = nodeBeforeNodeToBeDeleted;
+    }
+  }
+
+  void printForward() {
+    Node currentNode = this.head;
+    while (currentNode != null) {
+      print(currentNode.data);
+      currentNode = currentNode.nextNode;
+    }
+  }
+
+  void printBackwards() {
+    Node currentNode = this.tail;
+    while (currentNode != null) {
+      print(currentNode.data);
+      currentNode = currentNode.prevNode;
+    }
+  }
 }
--- Doubly-linked-list/lib/main.dart
+++ Doubly-linked-list/lib/main.dart
@@ -1,3 +1,22 @@
+import 'DoublyLinkedList.dart';
+
 void main(){
+  DoublyLinkedList newList = new DoublyLinkedList(1);
+  newList.insertAtLast(2);
+  newList.insertAtLast(345);
+  newList.insertAtLast(4);
+  newList.insertAtLast("jne");
+  //print(newList.count);
+ //print(newList.getNodeAt(5).data);
+  
+  
+  newList.insertAtPosition("middle child", 5);
+  
+  //print(newList.getNodeAt(1).nextNode.prevNode.data);
+  print("count: " +newList.count.toString());
+  print("forwards:");
+  newList.printForward();
+  print("backwards:");
+  newList.printBackwards();
   
 }
