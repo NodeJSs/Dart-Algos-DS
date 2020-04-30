9a006500a0da8aa7525e13cf0c9c7c08c6acd4e9
Yusuff Faruq <faruqyusuff437@gmail.com>
2020-04-30 23:27:06 +0000

    more code...

--- Circular-linked-list/lib/main.dart
+++ Circular-linked-list/lib/main.dart
@@ -1,15 +1,19 @@
 import 'CircularLinkedList.dart';
 import 'Node.dart';
 
-void main(){
+void main() {
+  //Solving the josephus problem:
+  /*
+  1600 people sit at a circular table. The 1st person has a knife and kills the 2nd, then hands it to the next person. Person 3 stabs 4 & gives the it to person 5. This is repeated until there is only a single person remaining. Who remains in the end?
+   */
   CircularLinkedList list = new CircularLinkedList(1);
-  for(var i = 2; i <= 6; i++){
+  for (var i = 2; i <= 1600; i++) {
     list.insertAtLast(i);
   }
   Node currentNode = list.head;
-  while(list.count != 1){
+  while (list.count != 1) {
     list.delete(currentNode.nextNode);
     currentNode = currentNode.nextNode;
   }
-  list.printForward(); 
+  list.printForward();
 }
